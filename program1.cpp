e325e4907d055b2f579fad14a3921bade0ced6ef
TarushiV26 <tarushiv2611@gmail.com>
2025-07-16 00:59:51 -0400

    program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,4 +1,4 @@
-//using and & or operators
+//using bitwise operators
 #include<iostream>
 using namespace std;
 int main()
@@ -10,7 +10,14 @@ int main()
     cin>>num2;
     int bitwise_and=num1&num2;
     int bitwise_or=num1|num2;
+    int bitwise_xor=num1^num2;
+    int bitwise_not=~num1;
+    int bitwise_leftshift=num1<<1;
+    int bitwise_rightshift=num2>>1;
     cout<<"binary AND operation: "<<bitwise_and<<endl;
     cout<<"binary OR operation: "<<bitwise_or<<endl;
-
+    cout<<"binary XOR operation: "<<bitwise_xor<<endl;
+    cout<<"binary NOT operation: "<<bitwise_not<<endl;
+    cout<<"binary LEFTSHIFT operation: "<<bitwise_leftshift<<endl;
+    cout<<"binary RIGHTSHIFT operation: "<<bitwise_rightshift<<endl;
 }
